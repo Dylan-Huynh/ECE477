11d0461ad04d1d8a79d7087a982bd6acd86cf159
Dylan-Huynh <77703791+Dylan-Huynh@users.noreply.github.com>
2023-03-27 19:53:40 -0400

    Add files via upload

--- LED_Matrix/main.c
+++ LED_Matrix/main.c
@@ -1,145 +1,202 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
+#include "stm32f0xx.h"
+#include <stdint.h>
+
+
+
+// Uncomment only one of the following to test each step
+//#define STEP41
+//#define STEP42
+//#define STEP43
+#define STEP44
+
+void init_usart2();
+
+void init_usart2() {
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // enables port A
+    GPIOA->MODER |= 0x20; // Enables PA2
+    // PA2 + USART2_TX = AF1
+    // PA3 + USART2_RX = AF1
+    GPIOA->AFR[0] |= 1<<8; // 0001 is AF1, and shift 8 bits left to reference pin 2
+
+    // enable clock for USART
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+    // disable USART2 by turning off UE bit
+    USART2->CR1 &= ~(USART_CR1_UE);
+
+    // set word size of 8 bits
+    USART2->CR1 &= ~(USART_CR1_M);
+
+    // set for one stop bit
+    USART2->CR2 &= ~(USART_CR2_STOP);
+
+    // set it for no parity
+    USART2->CR1 &= ~(USART_CR1_PCE);
+
+    // use 16x oversampling
+    USART2->CR1 &= ~(USART_CR1_OVER8);
+
+    // baud rate of 19200 (19.2k baud)
+    USART2->BRR = 0x9C4; // look at table 96 of the family reference manual
+
+    // enable transmitter and the receiver by setting the TE and RE bits
+    USART2->CR1 |= USART_CR1_TE;
+    //USART2->CR1 |= USART_CR1_RE;
+
+    // enable the USART
+    USART2->CR1 |= USART_CR1_UE;
+
+    // wait for the TE and RE bits to be acknowledged by checking the TEACK and REACK bits are both set in the ISR
+    // indicates that the USART is ready to transmit and receive
+    USART2->ISR |= USART_ISR_TEACK;
+    //USART2->ISR |= USART_ISR_REACK;
+
+}
+
+
+#ifdef STEP44
+
+#include <stdio.h>
+
+#define MESSAGESIZE 20
+
+#define MESSAGESIZE2 31
+
+unsigned char message[MESSAGESIZE] = {'B', '\x08A', '\x0A0', '\x0CA', '2', '\x0AC', '\x0A0',
+							'\b', '\1', '\0', '\x098', '(', '*', '\n', '0x0CC', '0x0A0', '0x0A0',
+							'\b', '2', 'B'};
+
+unsigned char message2[MESSAGESIZE] = {'\x08A', '\x08A', '\x08A', '\x08A', '\x08A', '\x08A', '\x08A',
+		'\x08A', '\x08A', '\x08A', '\x08A', '\x08A', '\x08A', '\x08A', '\x08A', '\x08A', '\x08A',
+		'\x08A', '\x08A', '\x08A'};
+
+uint8_t message3[MESSAGESIZE2] = {0x42, 0x54, 0x30, 0x31, 0x35, 0x01, 0x00, 0x00, 0x16, 0x41, 0x41, 0x33, 0x41, 0x00, 0x79,
+				 0x00, 0x61, 0x00, 0x62, 0x00, 0x61, 0x00, 0x68, 0x00, 0x69, 0x00, 0x74, 0x00, 0x6F, 0x1A};
+
+int count = 0;
+
+//uint8_t message[MESSAGESIZE] = {0xFE, 0x42, 0x8A, 0xA0, 0xCA, 0x32, 0xAC, 0xA0,
+//					   0x08, 0x02, 0x00, 0x98, 0x28, 0x2A, 0x0A, 0xCC, 0xA0,
+//					   0xA0, 0x08, 0x32, 0x42};
+
+//uint8_t message2[MESSAGESIZE] = {0x12, 0x12, 0x12, 0x12, 0x12, 0x12, 0x12, 0x12,
+//					   0x12, 0x12, 0x12, 0x12, 0x12, 0x12, 0x12, 0x12, 0x12,
+//					   0x12, 0x12, 0x12, 0x12};
+
+void enable_tty_interrupt(void) {
+    // Raise an interrupt every time the receive data register becomes not empty
+    // Remember to set the proper bit in the NVIC ISER as well
+    // Note that the name of the bit to set is difficult to determine. It is USART3_8_IRQn
+    USART2->CR1 |= USART_CR1_TXEIE;
+
+    // Trigger a DMA operation every time the receive data register becomes not empty
+    USART2->CR3 |= USART_CR3_DMAT;
+    NVIC->ISER[0] = 1<<USART2_IRQn;
+
+    // The subroutine should also enable the RCC clock for DMA Controller 2
+    RCC->AHBENR |= RCC_AHBENR_DMA2EN;
+    DMA2->RMPCR |= DMA2_CH1_USART2_TX;
+    DMA2_Channel1->CCR &= ~DMA_CCR_EN;  // First make sure DMA is turned off
+
+    // CMAR should be set to the address of serfifo
+    DMA2_Channel1->CMAR = (uint32_t)(message3); // configure memory address
+
+    // CPAR should be set to the address of the USART5->RDR
+    DMA2_Channel1->CPAR = (uint32_t)(&(USART2->TDR)); // Set CPAR to the address of the USART5->RDR register.
+
+    // CNDTR should be set to FIFOSIZE
+    DMA2_Channel1->CNDTR = MESSAGESIZE;
+
+    // The DIRection of copying should be from peripheral to memory
+    DMA2_Channel1->CCR |= DMA_CCR_DIR;
+
+    // Neither the total-completion nor the half-transfer interrupt should be enabled
+    DMA2_Channel1->CCR &= ~DMA_CCR_HTIE;
+    DMA2_Channel1->CCR &= ~DMA_CCR_TCIE;
+
+    // Both the MSIZE and the PSIZE should be set for 8 bits
+    //set memory datum size to 8bit
+    DMA2_Channel1->CCR &= ~DMA_CCR_MSIZE;
+    //set peripheral datum size to 8bit
+    DMA2_Channel1->CCR &= ~DMA_CCR_PSIZE;
+
+    // MINC should be set to increment the CMAR
+    DMA2_Channel1->CCR |= DMA_CCR_MINC; // increments the CMAR for every transfer
+
+    // PINC should not be set so that CPAR always points at the USART5->RDR.
+    DMA2_Channel1->CCR &= ~DMA_CCR_PINC;
+
+    // Enable CIRCular transfers.
+    DMA2_Channel1->CCR &= ~DMA_CCR_CIRC; //Set the channel for CIRCular operation.
+
+    // Do not enable MEM2MEM transfers.
+    DMA2_Channel1->CCR &= ~DMA_CCR_MEM2MEM; // memory to memory mode disabled
+
+    // Set the Priority Level to highest.
+    DMA2_Channel1->CCR |= DMA_CCR_PL;
+
+    //Finally, make sure that the channel is enabled for operation.
+    DMA2_Channel1->CCR |= DMA_CCR_EN; // enable DMA
+
+}
+
+/*
+// Works like line_buffer_getchar(), but does not check or clear ORE nor wait on new characters in USART
+char interrupt_getchar() {
+    // TODO
+
+    // USART_TypeDef *u = USART5;
+
+    // If we missed reading some characters, clear the overrun flag.
+    if (u->ISR & USART_ISR_ORE)
+        u->ICR |= USART_ICR_ORECF;
+
+    // Wait for a newline to complete the buffer.
+    while(fifo_newline(&input_fifo) == 0) {
+        asm volatile ("wfi"); // wait for an interrupt
+        while (!(u->ISR & USART_ISR_RXNE))
+            ;
+        insert_echo_char(u->RDR);
+    }
+    // Return a character from the line buffer.
+    char ch = fifo_remove(&input_fifo);
+    return ch;
+}
 */
 
-
-#include "stm32f0xx.h"
-#include <stdio.h>
-#include <math.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-			
-void init_usart1();
-void send_message();
-void USART_stole();
-
-void init_usart1() {
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // enables port B
-	GPIOB->MODER |= 0x0000a000; //Configures Ports PB6 and PB7 for AFR
-
-	// https://www.mouser.com/datasheet/2/389/dm00115237-1798191.pdf
-	// tables 14-19
-	// check for what pins can be connected to the USART's voia AFR's
-	//
-	// PC12 + USART5_TX = AF2
-	// PD2 + USART5_RX = AF2
-    // configure alternate function for AF2, need 0010 for AF2
-	// AFR[0] -> AFRL
-	// AFR[1] -> AFRH
-	GPIOB->AFR[0] |= 0x77000000; // 0010 is AF2, and shift 16 bits left to reference pin 12
-
-	// enable clock for USART
-	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
-
-	// disable USART5 by turning off UE bit
-	USART1->CR1 &= ~(USART_CR1_UE);
-
-	// set word size of 8 bits
-	USART1->CR1 &= ~(USART_CR1_M);
-
-	// set for one stop bit
-	USART1->CR2 &= ~(USART_CR2_STOP);
-
-	// set it for no parity
-	USART1->CR1 &= ~(USART_CR1_PCE);
-
-	// use 16x oversampling
-	USART1->CR1 &= ~(USART_CR1_OVER8);
-
-	// baud rate of 19200 (19.2k baud)
-	USART1->BRR = 0x9C4; // look at table 96 of the family reference manual
-
-	// enable transmitter and the receiver by setting the TE and RE bits
-	USART1->CR1 |= USART_CR1_TE;
-	USART1->CR1 |= USART_CR1_RE;
-
-	// enable the USART
-	USART1->CR1 |= USART_CR1_UE;
-
-	// wait for the TE and RE bits to be acknowledged by checking the TEACK and REACK bits are both set in the ISR
-	// indicates that the USART is ready to transmit and receive
-	USART1->ISR |= USART_ISR_TEACK;
-	USART1->ISR |= USART_ISR_REACK;
+int __io_putchar(char c) {
+    // TODO Copy from step 42
+    while(!(USART2->ISR & USART_ISR_TXE));
+    USART2->TDR = c;
+    return c;
 }
 
-void USART_stole() {
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // enables port B
-	GPIOB->MODER |= 0x0000a000; //Configures Ports PB6 and PB7 for AFR
-	GPIOB->AFR[0] |= 0x77000000;
-
-	USART_InitTypeDef USART_InitStruct;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-
-	/* Configure USART1 */
-	USART_InitStruct.USART_BaudRate = 19200;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Tx;
-	USART_Init(USART1, &USART_InitStruct);
-
-	/* Enable USART1 */
-	USART_Cmd(USART1, ENABLE);
+/*
+int __io_getchar(void) {
+    // TODO Use interrupt_getchar() instead of line_buffer_getchar()
+    int c = interrupt_getchar();
+    return c;
 }
+*/
 
-void send_message(void) {
-	USART_SendData(USART1, 0xFE);
-	USART_SendData(USART1, 0x42);
-	USART_SendData(USART1, 0x8A);
-	USART_SendData(USART1, 0xA0);
-	USART_SendData(USART1, 0xCA);
-	USART_SendData(USART1, 0x32);
-	USART_SendData(USART1, 0xAC);
-	USART_SendData(USART1, 0xA0);
-	//Last Universal
-	USART_SendData(USART1, 0x08);
-	USART_SendData(USART1, 0x02);
-	USART_SendData(USART1, 0x00);
-	USART_SendData(USART1, 0x98);
-	USART_SendData(USART1, 0x28);
-	USART_SendData(USART1, 0x2A);
-	USART_SendData(USART1, 0x0A);
-	USART_SendData(USART1, 0xCC);
-	USART_SendData(USART1, 0xA0);
-	USART_SendData(USART1, 0xA0);
-	USART_SendData(USART1, 0x08);
-	USART_SendData(USART1, 0x32);
-	USART_SendData(USART1, 0x42);
-
-}
-
-
-
-int main(void)
-{
-	//init_usart1();
-	USART_stole();
-	/*for(;;) {
-
-		if ((USART1->ISR & USART_ISR_TC) == USART_ISR_TC)
-		{
-			if (send == sizeof(stringtosend))
-			{
-				send=0;
-				//USART1->ICR |= USART_ICR_TCCF;
-			}
-			else
-			{
-				// clear transfer complete flag and fill TDR with a new char
-				USART1->TDR = stringtosend[send++];
-			}
-		}
-	}*/
-	send_message();
-	while (1){
-		USART_SendData(USART1, 0xFF);
+// name from startup_stm32.s
+void USART2_IRQHandler(void) {
+	if (count == MESSAGESIZE) {
+		count = 0;
 	}
+	//count++;
+	//int c = message2[count];
+	//USART2->TDR = c;
 }
+
+int main() {
+    init_usart2();
+    //enable_tty_interrupt();
+    for(;;) {
+    	if (count == MESSAGESIZE2) break;
+    	int c = message3[count];
+    	count++;
+    	__io_putchar(c);
+    }
+}
+#endif
