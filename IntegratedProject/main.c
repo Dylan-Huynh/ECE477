05b1b7739fbc36805be17335e6c1507485c6e8c7
Dylan-Huynh <huynh38@purdue.edu>
2023-04-07 15:55:12 -0400

    Update main.c

--- IntegratedProject/main.c
+++ IntegratedProject/main.c
@@ -17,6 +17,31 @@
 #define NUMLED (185)
 #define NUMTARGET (5)
 #define LEDSIZE (24*NUMLED + 600)
+#define MESSAGESIZE 42
+#define BLUETOOTHMESSAGESIZE 24
+#define BLUETOOTHMESSAGESIZE 28
+
+uint8_t gametimeMessage[MESSAGESIZE] = {0x42, 0x54, 0x30, 0x31, 0x35, 0x01, 0x00, 0x00, 0x21, 0x41, 0x41, 0x33, 0x41, 0x00, 0x39,
+				 	 	 	 	 0x00, 0x39, 0x00, 0x39, 0x00, 0x20, 0x00, 0x20, 0x00, 0x20, 0x00, 0x20, 0x00, 0x20, 0x00,
+								 0x20, 0x00, 0x20, 0x00, 0x30, 0x00, 0x30, 0x00, 0x30, 0x00, 0x30, 0x30};
+uint8_t titleMessage[MESSAGESIZE] = {0x42, 0x54, 0x30, 0x31, 0x35, 0x01, 0x00, 0x00, 0x21, 0x41, 0x41, 0x33, 0x41, 0x00, 0x59,
+				 	 	 	 	 0x00, 0x75, 0x00, 0x6b, 0x00, 0x69, 0x00, 0x20, 0x00, 0x4F, 0x00, 0x74, 0x00, 0x61, 0x00,
+								 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t gamefinishedMessage[MESSAGESIZE] = {0x42, 0x54, 0x30, 0x31, 0x35, 0x01, 0x00, 0x00, 0x21, 0x41, 0x41, 0x33, 0x41, 0x00, 0x47,
+				 	 	 	 	 0x00, 0x61, 0x00, 0x6d, 0x00, 0x65, 0x00, 0x20, 0x00, 0x4F, 0x00, 0x76, 0x00, 0x65, 0x00,
+								 0x00, 0x72, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t timescoreMessage[MESSAGESIZE] = {0x42, 0x54, 0x30, 0x31, 0x35, 0x01, 0x00, 0x00, 0x21, 0x41, 0x41, 0x33, 0x41, 0x00, 0x54,
+				 	 	 	 	 0x00, 0x49, 0x00, 0x4d, 0x00, 0x45, 0x00, 0x20, 0x00, 0x20, 0x00, 0x20, 0x00, 0x20, 0x00,
+								 0x20, 0x00, 0x53, 0x00, 0x43, 0x00, 0x4f, 0x00, 0x52, 0x00, 0x45, 0x00};
+
+uint8_t bluetoothGame2[BLUETOOTHMESSAGESIZE] = {0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x03,
+												0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00};
+
+uint8_t bluetoothGame2[BLUETOOTHMESSAGESIZE] = {0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x03,
+												0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00};
+
+uint8_t bluetoothGame3[BLUETOOTHMESSAGESIZE] = {0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x03,
+												0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x00};
 
 uint8_t ledstrip[LEDSIZE] = {0};
 uint8_t redChain[LEDSIZE] = {0};
@@ -86,6 +111,20 @@ static int target = -1; // neutral state
 static int gamemode = -1; // menu
 static int time_left = GAMETIME; // time for gamemode_2 - 1 second
 static int targets_hit = 0; // amount of targets hit in gamemode_2
+
+// Bluetooth message data
+static uint8_t target1_hit = 0;
+static uint8_t target1_miss = 0;
+static uint8_t target2_hit = 0;
+static uint8_t target2_miss = 0;
+static uint8_t target3_hit = 0;
+static uint8_t target3_miss = 0;
+static uint8_t target4_hit = 0;
+static uint8_t target4_miss = 0;
+static uint8_t target5_hit = 0;
+static uint8_t target5_miss = 0;
+static uint8_t blade_hit = 0;
+static uint8_t blade_miss = 0;
 
 static void nano_wait(unsigned int n) {
     asm(    "        mov r0,%0\n"
@@ -210,6 +249,7 @@ void menu_setup() {
     nano_wait(1000000000);
     GPIOB->BSRR = 0x300;
     Build_LED_Targets(ledstrip, purple, purple, off, off, off);
+	send_LED_message[titleMessage];
 }
 
 void init_tim1(void) {
@@ -307,6 +347,141 @@ void Build_LED_Targets(uint8_t *ledstrip, uint8_t *color1, uint8_t *color2, uint
 	DMA1_Channel2->CCR |= DMA_CCR_EN;
 }
 
+void init_usart1() {
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // enables port A
+    GPIOB->MODER |= 0x2000; // Enables PB6
+    // PA2 + USART2_TX = AF1
+    // PA3 + USART2_RX = AF1
+    GPIOA->AFR[0] |= 1<<8; // 0001 is AF1, and shift 8 bits left to reference pin 2
+
+    // enable clock for USART
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+    // disable USART2 by turning off UE bit
+    USART2->CR1 &= ~(USART_CR1_UE);
+
+    // set word size of 8 bits
+    USART2->CR1 &= ~(USART_CR1_M);
+
+    // set for one stop bit
+    USART2->CR2 &= ~(USART_CR2_STOP);
+
+    // set it for no parity
+    USART2->CR1 &= ~(USART_CR1_PCE);
+
+    // use 16x oversampling
+    USART2->CR1 &= ~(USART_CR1_OVER8);
+
+    // baud rate of 19200 (19.2k baud)
+    USART2->BRR = 0x9C4; // look at table 96 of the family reference manual
+
+    // enable transmitter and the receiver by setting the TE and RE bits
+    USART2->CR1 |= USART_CR1_TE;
+    //USART2->CR1 |= USART_CR1_RE;
+
+    // enable the USART
+    USART2->CR1 |= USART_CR1_UE;
+
+    // wait for the TE and RE bits to be acknowledged by checking the TEACK and REACK bits are both set in the ISR
+    // indicates that the USART is ready to transmit and receive
+    USART2->ISR |= USART_ISR_TEACK;
+    //USART2->ISR |= USART_ISR_REACK;
+
+}
+
+void init_usart2() {
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // enables port A
+    GPIOA->MODER |= 0x20; // Enables PA2
+    // PA2 + USART2_TX = AF1
+    // PA3 + USART2_RX = AF1
+    GPIOA->AFR[0] |= 1<<8; // 0001 is AF1, and shift 8 bits left to reference pin 2
+
+    // enable clock for USART
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+    // disable USART2 by turning off UE bit
+    USART2->CR1 &= ~(USART_CR1_UE);
+
+    // set word size of 8 bits
+    USART2->CR1 &= ~(USART_CR1_M);
+
+    // set for one stop bit
+    USART2->CR2 &= ~(USART_CR2_STOP);
+
+    // set it for no parity
+    USART2->CR1 &= ~(USART_CR1_PCE);
+
+    // use 16x oversampling
+    USART2->CR1 &= ~(USART_CR1_OVER8);
+
+    // baud rate of 19200 (19.2k baud)
+    USART2->BRR = 0x9C4; // look at table 96 of the family reference manual
+
+    // enable transmitter and the receiver by setting the TE and RE bits
+    USART2->CR1 |= USART_CR1_TE;
+    //USART2->CR1 |= USART_CR1_RE;
+
+    // enable the USART
+    USART2->CR1 |= USART_CR1_UE;
+
+    // wait for the TE and RE bits to be acknowledged by checking the TEACK and REACK bits are both set in the ISR
+    // indicates that the USART is ready to transmit and receive
+    USART2->ISR |= USART_ISR_TEACK;
+    //USART2->ISR |= USART_ISR_REACK;
+
+}
+
+void send_LED_message(uint8_t arr) {
+    // TODO Copy from step 42
+	for(int i =0; i <= MESSAGESIZE; i++) {
+		while(!(USART2->ISR & USART_ISR_TXE));
+		USART2->TDR = arr[i];
+	}
+}
+
+void change_score(int n) {
+
+
+	int n_one = n % 10;
+	int n_ten = (n % 100) / 10;
+	int n_hundred = (n % 1000) / 100;
+	int n_thousand = n / 1000;
+
+	gametimeMessage[40] = n_one + 48;
+	gametimeMessage[38] = n_ten + 48;
+	gametimeMessage[36] = n_hundred + 48;
+	gametimeMessage[34] = n_thousand+ 48;
+}
+
+void change_time(int n) {
+	int n_one = n % 10;
+	int n_ten = (n % 100) / 10;
+	int n_hundred = (n % 1000) / 100;
+
+	gametimeMessage[18] = n_one + 48;
+	gametimeMessage[16] = n_ten + 48;
+	gametimeMessage[14] = n_hundred + 48;
+}
+
+void load_and_send_bluetooth2() {
+	bluetoothGame2[1] = target1_hit;
+	bluetoothGame2[2] = target1_miss;
+	bluetoothGame2[4] = target1_hit;
+	bluetoothGame2[5] = target1_miss;
+	bluetoothGame2[8] = target1_hit;
+	bluetoothGame2[9] = target1_miss;
+	bluetoothGame2[12] = target1_hit;
+	bluetoothGame2[13] = target1_miss;
+	bluetoothGame2[16] = target1_hit;
+	bluetoothGame2[17] = target1_miss;
+	bluetoothGame2[20] = blade1_hit;
+	bluetoothGame2[21] = blade1_miss;
+	for(int i =0; i <= BLUETOOTHMESSAGESIZE; i++) {
+		while(!(USART1->ISR & USART_ISR_TXE));
+		USART1->TDR = arr[i];
+	}
+}
+
 void init_tim2() {
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
     TIM2->CR1 &= ~TIM_CR1_CEN;
@@ -321,16 +496,21 @@ void TIM2_IRQHandler() {
     TIM2->SR = ~TIM_SR_UIF;
     if (time_left) {
         time_left--;
-        display_handle(time_left);
+		change_time(time_left);
+		change_score(targets_hit);
+		send_LED_message(gametimeMessage);
+        //display_handle(time_left);
     }
     else {
         gamemode = -1;
         time_left = GAMETIME;
         GPIOB->BRR = 0xFFF;
         Build_LED_Targets(ledstrip, red, red, red, red, red);
+		send_LED_message(gamefinishedMessage);
         TIM2->CR1 &= ~TIM_CR1_CEN;
         nano_wait(1000000000);
-        display_handle(targets_hit);
+		send_LED_message(gametimeMessage);
+        //display_handle(targets_hit);
         nano_wait(1000000000);
         menu_setup();
     }
@@ -371,8 +551,13 @@ void EXTI0_1_IRQHandler() {
             gamemode = 2;
             GPIOB->BRR = 0xFFF;
             Build_LED_Targets(ledstrip, white, white, white, white, white);
+			send_LED_message(gamefinishedMessage);
             nano_wait(1000000000);
-            display_handle(time_left);
+			load_bluetooth_message();
+			change_time(time_left);
+			change_score(targets_hit);
+			send_LED_message(gametimeMessage);
+            //display_handle(time_left);
             TIM2->CR1 |= TIM_CR1_CEN;
             pick_target();
         }
